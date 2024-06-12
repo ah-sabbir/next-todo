1e791559cc7df7b53e88b580deb05a7d4842c488
sabbir ahmmed <ahsabbir103@gmail.com>
2024-06-13 01:05:38 +0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -24,18 +24,6 @@ import { graphqlClient } from "@/lib/gqlClient";
 import TodoModal from "@/components/todo/TodoModal";
 
 
-// const GET_TODOS = gql`
-// query {
-//   todos(where: {user_id: {_eq: "bd2b1838-9145-498f-9bec-42e8e77810b6"}}) {
-//     title
-//     todo_id
-//     user_id
-//     updated_at
-//     description
-//   }
-// }
-// `
-
 function App() {
   return (
     <NhostProvider nhost={nhost}>
@@ -48,7 +36,16 @@ function App() {
 
 export default function Home() {
 
-  const [newTask, setNewTask] = useState<boolean>(false);
+  const [Tasks, setTasks] = useState<any>();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
 
   const { data, loading } = useQuery(GET_TODOS("bd2b1838-9145-498f-9bec-42e8e77810b6"), {
     context: {
@@ -62,8 +59,10 @@ export default function Home() {
     return <div>loading.....</div>;
   }
 
-    console.log(data.todos);
-
+  // useEffect(()=>{
+  //     if(!data.todos) return; 
+  //     setTasks(data.todos)
+  // },[])
 
 
 
@@ -71,17 +70,17 @@ export default function Home() {
     <main className="w-full min-h-screen flex flex-col items-center gap-3 px-5 my-auto md:px-20 md:my-10">
       <h3 className="text-left w-full text-[24px] font-semibold leading-[20px]none border-b-2 border-zinc-100 py-5 my-10">MY To-Do</h3>
       <div className="w-full flex flex-col md:flex-row gap-2 md:gap-5">
-        <button className="bg-[#3B40D5] text-zinc-100 text-[14px] px-3 py-2 md:px-5 md:py-3 rounded-md" onClick={()=>setNewTask(!newTask)}>+ New Task</button>
-        <TodoModal />
+        <button className="bg-[#3B40D5] text-zinc-100 text-[14px] px-3 py-2 md:px-5 md:py-3 rounded-md" onClick={handleOpenModal}>+ New Task</button>
+        {isOpen && <TodoModal isOpen={isOpen} onClose={handleCloseModal} isUpdate={false}/>}
         <div className="filter-btn flex items-center gap-3 justify-center border-2 rounded-md px-6 py-2 cursor-pointer">
           <svg className="w-4 h-4 text-zinc-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z"/>
           </svg>
           <span>Filter</span>
         </div>
-        <button className="bg-[#3B40D5] text-zinc-100 text-[14px] px-3 py-2 md:px-5 md:py-3 rounded-md" > select multiple</button>
+        <button className="bg-[#3B40D5] text-zinc-100 text-[14px] px-3 py-2 md:px-5 md:py-3 rounded-md" > Trash</button>
       </div>
-        <div className="w-full flex flex-wrap ">
+        <div className="w-full flex flex-wrap">
           {
             data.todos && data.todos.map((todo:any, i:any)=>(
               <Todo key={i} data = {todo} title="this is title" description="lorem ipsum lorem ipsum"/>
--- components/todo/TodoModal.tsx
+++ components/todo/TodoModal.tsx
@@ -1,5 +1,5 @@
 import { insertTodo } from '@/actions/todos/insertTodo';
-import React from 'react'
+import React, { useState } from 'react'
 
 
 
@@ -24,10 +24,11 @@ mutation InsertTask($object: todos_insert_input!) {
 //"bd2b1838-9145-498f-9bec-42e8e77810b6"
 
 
-const TodoModal = (show:Boolean) => {
+const TodoModal = ({onClose, isOpen, isUpdate}:any) => {
 
-    const [title, setTitle] = React.useState('');
-    const [content, setContent] = React.useState('');
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [close, setClose] = useState(1);
   
     const [InsertTask, { loading, error }] = useMutation(query, {
       context: {
@@ -39,21 +40,26 @@ const TodoModal = (show:Boolean) => {
   
     const handleSubmit =  (e:any) => {
       e.preventDefault();
-      InsertTask({ variables: { object:{ title: title, description: content, status: "active", user_id: "bd2b1838-9145-498f-9bec-42e8e77810b6" } } }) //
-        .then((response) => {
-          console.log(response.data);
-          // Handle successful mutation
-        })
-        .catch((error) => {
-          console.error(error.message);
-          // Handle error
-        });
+      if(content!==''){
+        InsertTask({ variables: { object:{ title: title, description: content, status: "active", user_id: "bd2b1838-9145-498f-9bec-42e8e77810b6" } } }) //
+          .then((response) => {
+            // console.log(response.data);
+            // Handle successful mutation
+            onClose();
+          })
+          .catch((error) => {
+            console.error(error.message);
+            // Handle error
+          });
+      }
+
+      onClose();
     };
 
 
 
   return (
-        <div id="default-modal" tabIndex={-1} aria-hidden="true" className="block bg-transparent bg-zinc-600/50 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div id="default-modal" tabIndex={-1} aria-hidden="true" className={`${isOpen ? 'block': 'hidden'} bg-transparent bg-zinc-600/50 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
             <div className="relative p-4 w-full max-w-2xl max-h-full">
             <form onSubmit={handleSubmit} className=" w-[600px] bg-zinc-100 px-3 py-2 mb-8 shadow rounded text-gray-600">
                 <div className="top flex justify-between items-center mb-3">
@@ -83,7 +89,7 @@ const TodoModal = (show:Boolean) => {
                     </div>
                     </div>
                     <div className="right flex-1 flex items-center justify-end">
-                    <button className="text-sm close-btn">Close</button>
+                    <button className="text-sm close-btn" onClick={onClose}>Close</button>
                     </div>
                 </div>
                 </form>
